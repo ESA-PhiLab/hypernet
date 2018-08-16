64bc9019433360c71ba33a59f9c1b5838d1fa290
Michał Myller <mmyller@kplabs.pl>
2018-08-16 16:15:47 +0200

    Included saving best model and saving metrics to csv

--- python_research/augmentation/GAN.py
+++ python_research/augmentation/GAN.py
@@ -1,8 +1,9 @@
 import argparse
 import os
 import numpy as np
-from torch.autograd import Variable
+from time import time
 
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.autograd as autograd
@@ -13,7 +14,6 @@ from python_research.augmentation.generator import Generator
 from python_research.augmentation.classifier import Classifier
 from python_research.augmentation.dataset import HyperspectralDataset
 
-os.makedirs('GAN', exist_ok=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset_path', type=str, help='Path to the dataset in .npy format')
@@ -21,21 +21,18 @@ parser.add_argument('--gt_path', type=str, help='Path to the ground truth file i
 parser.add_argument('--artifacts_path', type=str, help='Path in which artifacts will be stored')
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
 parser.add_argument('--batch_size', type=int, default=64, help='size of the batches')
-parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
-parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
-parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of first order momentum of gradient')
-parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-parser.add_argument('--channels', type=int, default=1, help='number of image channels')
 parser.add_argument('--n_critic', type=int, default=2, help='number of training steps for discriminator per iter')
-parser.add_argument('--clip_value', type=float, default=0.01, help='lower and upper clip value for disc. weights')
-parser.add_argument('--sample_interval', type=int, default=400, help='interval betwen image samples')
+parser.add_argument('--patience', type=int, default=50, help='Number of epochs withour improvement on generator loss after which training will be terminated')
 opt = parser.parse_args()
 print(opt)
 
 CLASS_LABEL = 1
+METRICS_PATH = os.path.join(opt.artifacts_path, "metrics.csv")
+BEST_MODEL_PATH = os.path.join(opt.artifacts_path, "best_model")
+
+os.makedirs(opt.artifacts_path, exist_ok=True)
 
 cuda = True if torch.cuda.is_available() else False
-
 # Loss weight for gradient penalty
 lambda_gp = 10
 
@@ -61,17 +58,18 @@ optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=0.001, centered
 optimizer_C = torch.optim.RMSprop(classifier.parameters(), lr=0.001, centered=True)
 classifier_criterion = nn.CrossEntropyLoss()
 
-Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
 def compute_gradient_penalty(D, real_samples, fake_samples):
     """Calculates the gradient penalty loss for WGAN GP"""
     # Random weight term for interpolation between real and fake samples
-    alpha = Tensor(np.random.random((real_samples.size(0), 1)))
+    alpha = FloatTensor(np.random.random((real_samples.size(0), 1)))
     # Get random interpolation between real and fake samples
     interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
     d_interpolates = D(interpolates)
-    fake = Variable(Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
+    fake = Variable(FloatTensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
     # Get gradient w.r.t. interpolates
     gradients = autograd.grad(outputs=d_interpolates, inputs=interpolates,
                               grad_outputs=fake, create_graph=True, retain_graph=True,
@@ -79,22 +77,23 @@ def compute_gradient_penalty(D, real_samples, fake_samples):
     gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
     return gradient_penalty
 
+
+batches_done = 0
+best_generator_loss = np.inf
+epochs_without_improvement = 0
 # ----------
 #  Training
 # ----------
-
-
-d_losses = []
-c_losses = []
-g_losses = []
-batches_done = 0
 for epoch in range(opt.n_epochs):
+    d_losses = []
+    c_losses = []
+    g_losses = []
+    epoch_start = time()
     for i, (samples, labels) in enumerate(dataloader):
-
         # Configure input
-        real_samples = Variable(samples.type(torch.cuda.FloatTensor))
+        real_samples = Variable(samples.type(FloatTensor))
         batch_size = len(real_samples)
-        labels = Variable(labels.type(torch.cuda.LongTensor))
+        labels = Variable(labels.type(LongTensor))
         # ---------------------
         #  Train Discriminator and Classifier
         # ---------------------
@@ -103,10 +102,10 @@ for epoch in range(opt.n_epochs):
         optimizer_C.zero_grad()
 
         # Sample noise as generator input
-        noise = Variable(Tensor(np.random.normal(0, 1, (batch_size, input_shape))).type(torch.cuda.FloatTensor))
+        noise = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, input_shape))))
 
         # Generate a batch of samples
-        reshaped_labels = torch.reshape(labels, (labels.shape[0], 1)).type(torch.cuda.FloatTensor)
+        reshaped_labels = torch.reshape(labels, (labels.shape[0], 1)).type(FloatTensor)
         noise_with_labels = torch.cat([noise, reshaped_labels], dim=1)
         fake_samples = generator(noise_with_labels)
 
@@ -116,11 +115,10 @@ for epoch in range(opt.n_epochs):
 
         # Fake samples
         fake_discriminator_validity = discriminator(fake_samples)
-        fake_classifier_validity = classifier(Variable(fake_samples.data, requires_grad=True))
         # Gradient penalty
         gradient_penalty = compute_gradient_penalty(discriminator, real_samples.data, fake_samples.data)
         # Adversarial loss
-        c_loss = classifier_criterion(real_classifier_validity, labels) + classifier_criterion(fake_classifier_validity, labels)
+        c_loss = classifier_criterion(real_classifier_validity, labels)
         d_loss = -torch.mean(real_discriminator_validity) + torch.mean(fake_discriminator_validity) + lambda_gp * gradient_penalty
 
         d_loss.backward()
@@ -140,17 +138,29 @@ for epoch in range(opt.n_epochs):
 
             # Generate a batch of images
             fake_samples = generator(noise_with_labels)
-            # Loss measures generator's ability to fool the discriminator
+            # Loss measures generator's ability to fool the discriminator and generate sample with correct class
             # Train on fake images
             fake_discriminator_validity = discriminator(fake_samples)
-            g_loss = -torch.mean(fake_discriminator_validity)
+            fake_classifier_validity = classifier(fake_samples)
+            g_loss = -torch.mean(fake_discriminator_validity) + classifier_criterion(fake_classifier_validity, labels)
 
             g_loss.backward()
             optimizer_G.step()
             g_losses.append(g_loss.item())
 
             batches_done += opt.n_critic
+    epoch_duration = time() - epoch_start
+    metrics = open(METRICS_PATH, 'a')
+    metrics.write("{},{},{},{},{}\n".format(epoch, epoch_duration, np.average(d_losses), np.average(g_losses), np.average(c_losses)))
+    metrics.close()
+    if best_generator_loss > np.average(g_losses):
+        torch.save(generator.state_dict(), BEST_MODEL_PATH)
+        epochs_without_improvement = 0
+    else:
+        epochs_without_improvement += 1
+        if epochs_without_improvement >= opt.patience:
+            print("{} epochs without improvement, terminating".format(opt.patience))
+            break
     print("[Epoch {}/{}] [D loss {}] [G loss {}] [C loss {}]".format(epoch, opt.n_epochs, np.average(d_losses), np.average(g_losses), np.average(c_losses)))
 
-os.makedirs(opt.artifacts_path, exist_ok=True)
-torch.save(generator.state_dict(), os.path.join(opt.artifacts_path, "generator_model"))
+
