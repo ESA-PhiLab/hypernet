b7f00b3e1775cc095a60adfe93531775a18f0926
Michał Myller <mmyller@kplabs.pl>
2018-08-20 11:19:37 +0200

    small fixes

--- python_research/augmentation/GAN.py
+++ python_research/augmentation/GAN.py
@@ -21,26 +21,26 @@ parser.add_argument('--gt_path', type=str, help='Path to the ground truth file i
 parser.add_argument('--artifacts_path', type=str, help='Path in which artifacts will be stored')
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
 parser.add_argument('--batch_size', type=int, default=64, help='size of the batches')
-parser.add_argument('--n_critic', type=int, default=2, help='number of training steps for discriminator per iter')
+parser.add_argument('--n_critic', type=int, default=3, help='number of training steps for discriminator per iter')
 parser.add_argument('--patience', type=int, default=50, help='Number of epochs withour improvement on generator loss after which training will be terminated')
 parser.add_argument('--verbose', type=bool, help="If True, metric will be printed after each epoch")
-opt = parser.parse_args()
-if opt.verbose:
-    print(opt)
+args = parser.parse_args()
+if args.verbose:
+    print(args)
 
 CLASS_LABEL = 1
-METRICS_PATH = os.path.join(opt.artifacts_path, "metrics.csv")
-BEST_MODEL_PATH = os.path.join(opt.artifacts_path, "best_model")
+METRICS_PATH = os.path.join(args.artifacts_path, "metrics.csv")
+BEST_MODEL_PATH = os.path.join(args.artifacts_path, "best_model")
 
-os.makedirs(opt.artifacts_path, exist_ok=True)
+os.makedirs(args.artifacts_path, exist_ok=True)
 
 cuda = True if torch.cuda.is_available() else False
 # Loss weight for gradient penalty
 lambda_gp = 10
 
 # Configure data loader
-transformed_dataset = HyperspectralDataset(opt.dataset_path, opt.gt_path)
-dataloader = DataLoader(transformed_dataset, batch_size=opt.batch_size, shuffle=True, drop_last=True)
+transformed_dataset = HyperspectralDataset(args.dataset_path, args.gt_path)
+dataloader = DataLoader(transformed_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
 
 input_shape = transformed_dataset.x.shape[-1]
 
@@ -55,9 +55,9 @@ if cuda:
     classifier.cuda()
 
 # Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=0.001)
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=0.001)
-optimizer_C = torch.optim.Adam(classifier.parameters(), lr=0.001)
+optimizer_G = torch.optim.RMSprop(generator.parameters(), lr=0.0001, centered=True)
+optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=0.0001, centered=True)
+optimizer_C = torch.optim.RMSprop(classifier.parameters(), lr=0.001, centered=True)
 classifier_criterion = nn.CrossEntropyLoss()
 
 FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
@@ -88,10 +88,14 @@ epochs_without_improvement = 0
 # ----------
 #  Training
 # ----------
-for epoch in range(opt.n_epochs):
+for epoch in range(args.n_epochs):
     d_losses = []
     c_losses = []
     g_losses = []
+    real = []
+    fake = []
+    g_fake = []
+    g_class = []
     epoch_start = time()
     for i, (samples, labels) in enumerate(dataloader):
         # Configure input
@@ -123,8 +127,9 @@ for epoch in range(opt.n_epochs):
         gradient_penalty = compute_gradient_penalty(discriminator, real_samples.data, fake_samples.data)
         # Adversarial loss
         c_loss = classifier_criterion(real_classifier_validity, labels)
-        d_loss = -torch.mean(real_discriminator_validity) + torch.mean(fake_discriminator_validity) + lambda_gp * gradient_penalty
-
+        d_loss = torch.mean(real_discriminator_validity) - torch.mean(fake_discriminator_validity) + lambda_gp * gradient_penalty
+        real.append(torch.mean(real_discriminator_validity).detach().numpy())
+        fake.append(torch.mean(fake_discriminator_validity).detach().numpy())
         d_loss.backward()
         c_loss.backward()
 
@@ -132,8 +137,12 @@ for epoch in range(opt.n_epochs):
         optimizer_C.step()
         d_losses.append(d_loss.item())
         c_losses.append(c_loss.item())
+
+        for p in discriminator.parameters():
+            p.data.clamp_(-0.01, 0.01)
+
         # Train the generator every n_critic steps
-        if i % opt.n_critic == 0:
+        if i % args.n_critic == 0:
 
             optimizer_G.zero_grad()
             # -----------------
@@ -146,13 +155,14 @@ for epoch in range(opt.n_epochs):
             # Train on fake images
             fake_discriminator_validity = discriminator(fake_samples)
             fake_classifier_validity = classifier(fake_samples)
-            g_loss = -torch.mean(fake_discriminator_validity) + classifier_criterion(fake_classifier_validity, labels)
-
+            g_loss = abs(torch.mean(fake_discriminator_validity)) + classifier_criterion(fake_classifier_validity, labels)
+            g_fake.append(torch.mean(fake_discriminator_validity).detach().numpy())
+            g_class.append(classifier_criterion(fake_classifier_validity, labels).detach().numpy())
             g_loss.backward()
             optimizer_G.step()
             g_losses.append(g_loss.item())
 
-            batches_done += opt.n_critic
+            batches_done += args.n_critic
     epoch_duration = time() - epoch_start
     metrics = open(METRICS_PATH, 'a')
     metrics.write("{},{},{},{},{}\n".format(epoch,
@@ -169,14 +179,18 @@ for epoch in range(opt.n_epochs):
         epochs_without_improvement = 0
     else:
         epochs_without_improvement += 1
-        if epochs_without_improvement >= opt.patience:
-            print("{} epochs without improvement, terminating".format(opt.patience))
+        if epochs_without_improvement >= args.patience:
+            print("{} epochs without improvement, terminating".format(args.patience))
             break
-    if opt.verbose:
-        print("[Epoch {}/{}] [D loss {}] [G loss {}] [C loss {}]".format(epoch,
-                                                                         opt.n_epochs,
+    if args.verbose:
+        print("[Epoch {}/{}] [D loss {}] [G loss {}] [C loss {}] [real: {} fake: {}] [g_fake {} g_class {}]".format(epoch,
+                                                                         args.n_epochs,
                                                                          np.average(d_losses),
                                                                          generator_loss,
-                                                                         np.average(c_losses)))
+                                                                         np.average(c_losses),
+                                                                         np.average(real),
+                                                                         np.average(fake),
+                                                                         np.average(g_fake),
+                                                                         np.average(g_class)))
 
 
