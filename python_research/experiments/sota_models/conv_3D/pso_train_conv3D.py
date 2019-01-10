2bb01ccc84cbe2d0adf79da266fcc3b4d4003d57
Marek Antoniak <mantoniak@kplabs.pl>
2019-01-10 15:14:21 +0100

    Added jupyter for Conv 3D PSO

--- python_research/experiments/sota_models/conv_3D/pso_train_conv3D.py
+++ python_research/experiments/sota_models/conv_3D/pso_train_conv3D.py
@@ -9,13 +9,41 @@ from python_research.experiments.sota_models.utils.monte_carlo import prep_monte
 from python_research.experiments.sota_models.utils.models_runner import run_model
 from python_research.fastPSO.pso import Pso, Particle, Bounds
 
+from typing import List, NamedTuple
+
+
+class Arguments(NamedTuple):
+    """
+    Container for PSO runner arguments
+    """
+    run_idx: str
+    dtype: str
+    cont: str
+    epochs: int
+    data_set: str
+    data_name: str
+    min_neighbourhood_size: int
+    max_neighbourhood_size: int
+    labels: str
+    batch: int
+    patience: int
+    dest_path: str
+    classes: int
+    test_size: float
+    val_size: float
+    min_channels: List
+    max_channels: List
+    channels_step: List
+    input_depth: int
+    swarm_size: int
+
 
 class PsoRunner:
     """
     PSO runner for 3D convolutional network.
     """
 
-    def __init__(self, args):
+    def __init__(self, args: Arguments):
         """
         Initialization.
         :param args: Arguments for runner.
@@ -24,6 +52,11 @@ class PsoRunner:
         self.archive = {}
 
     def _extract_parameters(self, position):
+        """
+        Extracts parameters for particle
+        :param position: Position of the particle.
+        :return: Particle parameters.
+        """
         swarm_neighbourhood_size, *swarm_channels = position
         neighbourhood_size = int(swarm_neighbourhood_size)
         if neighbourhood_size % 2 == 0:
@@ -35,6 +68,11 @@ class PsoRunner:
         return neighbourhood_size, channels
 
     def _objective_function(self, particle: Particle):
+        """
+        PSO objective function
+        :param particle: Particle data.
+        :return: Particle score.
+        """
         neighbourhood_size, channels = self._extract_parameters(particle.position())
         print('Processing: neighbourhood = {}, channels = {}'.format(neighbourhood_size, channels))
 
@@ -42,7 +80,9 @@ class PsoRunner:
         if archive_index in self.archive:
             return self.archive[archive_index]
 
-        args = copy.deepcopy(self.args)
+        args = lambda: None
+        for field in self.args._fields:
+            setattr(args, field, getattr(self.args, field))
         args.neighbourhood_size = neighbourhood_size
         args.input_dim = [args.input_depth, neighbourhood_size, neighbourhood_size]
         args.channels = channels
@@ -126,35 +166,32 @@ class PsoRunner:
         return (best_neighbourhood, best_channels)
 
 
-def arguments():
+def arguments() -> Arguments:
     """
     Arguments parser.
     :return: Parsed arguments.
     """
     parser = argparse.ArgumentParser(description='Arguments for runner.')
-    parser.add_argument('--run_idx', dest='run_idx', help='Run index.')
-    parser.add_argument('--dtype', dest='dtype', help='Data type.')
-    parser.add_argument('--cont', dest='cont', help='Contamination file for bands selection.', type=str)
-    parser.add_argument('--epochs', dest='epochs', help='Number of epochs.', type=int)
-    parser.add_argument('--data_set', dest='data_set', help='Path to the data set.')
-    parser.add_argument('--data_name', dest='data_name', help='Name of the data set.')
-    parser.add_argument('--min_neighbourhood_size', dest='min_neighbourhood_size', help='Min spatial size of the patch.', type=int)
-    parser.add_argument('--max_neighbourhood_size', dest='max_neighbourhood_size', help='Max spatial size of the patch.', type=int)
-    parser.add_argument('--labels', dest='labels', help='Path to labels.')
-    parser.add_argument('--batch', dest='batch', help='Batch size.', type=int)
-    parser.add_argument('--patience', dest='patience', help='Number of epochs without improvement.')
-    parser.add_argument('--dest_path', dest='dest_path', help='Destination of the artifacts folder.')
-    parser.add_argument('--classes', dest='classes', help='Number of classes.', type=int)
-    parser.add_argument('--test_size', dest='test_size', help='Test size.', type=float)
-    parser.add_argument('--val_size', dest='val_size', help='Validation size.', type=float)
-    parser.add_argument('--min_channels', dest='min_channels', nargs='+', help='List of min channels.',
-                        required=True)
-    parser.add_argument('--max_channels', dest='max_channels', nargs='+', help='List of max channels.',
-                        required=True)
-    parser.add_argument('--channels_step', dest='channels_step', nargs='+', help='List of step value between min and max channels.',
-                        required=True)
-    parser.add_argument('--input_depth', dest='input_depth', help='Input depth dimensionality.', type=int)
-    parser.add_argument('--swarm_size', dest='swarm_size', help='Swarm size.', type=int)
+    parser.add_argument('--run_idx', dest='run_idx', help='Run index.', required=True)
+    parser.add_argument('--dtype', dest='dtype', help='Data type used by the model.', required=True)
+    parser.add_argument('--cont', dest='cont', help='Path to file containing indexes of selected bands.', type=str)
+    parser.add_argument('--epochs', dest='epochs', help='Number of epochs.', type=int, required=True)
+    parser.add_argument('--data_set', dest='data_set', help='Path to the data set.', required=True)
+    parser.add_argument('--data_name', dest='data_name', help='Name of the data set.', required=True)
+    parser.add_argument('--min_neighbourhood_size', dest='min_neighbourhood_size', help='Min spatial size of the patch.', type=int, required=True)
+    parser.add_argument('--max_neighbourhood_size', dest='max_neighbourhood_size', help='Max spatial size of the patch.', type=int, required=True)
+    parser.add_argument('--labels', dest='labels', help='Path to labels.', required=True)
+    parser.add_argument('--batch', dest='batch', help='Batch size.', type=int, required=True)
+    parser.add_argument('--patience', dest='patience', help='Number of epochs without improvement.', required=True)
+    parser.add_argument('--dest_path', dest='dest_path', help='Destination of the artifacts folder.', required=True)
+    parser.add_argument('--classes', dest='classes', help='Number of classes.', type=int, required=True)
+    parser.add_argument('--test_size', dest='test_size', help='Test size.', type=float, required=True)
+    parser.add_argument('--val_size', dest='val_size', help='Validation size.', type=float, required=True)
+    parser.add_argument('--min_channels', dest='min_channels', nargs='+', help='List of min channels.', required=True)
+    parser.add_argument('--max_channels', dest='max_channels', nargs='+', help='List of max channels.', required=True)
+    parser.add_argument('--channels_step', dest='channels_step', nargs='+', help='List of step value between min and max channels.', required=True)
+    parser.add_argument('--input_depth', dest='input_depth', help='Input depth dimensionality.', type=int, required=True)
+    parser.add_argument('--swarm_size', dest='swarm_size', help='Swarm size.', type=int, required=True)
     return parser.parse_args()
 
 
