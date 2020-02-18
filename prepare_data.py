338b0de539f393e281098cd8517e5332e5daad5e
Michał Myller <mmyller@kplabs.pl>
2020-02-18 08:42:53 +0100

    first version without tests

---
+++ prepare_data.py
@@ -0,0 +1,71 @@
+"""
+Load the data, reformat it to have right dimensions, split it into train, test 
+and val and sets and save them in .h5 file with 'train', 'val' and 'test' keys.
+"""
+
+import os
+import h5py
+
+import clize
+
+from input_output import load_npy
+from transformations import reshape_to_1D_samples
+from utils import Dataset, train_val_test_split
+
+EXTENSION = 1
+
+
+def main(*,
+         data_file_path: str,
+         ground_truth_path: str,
+         output_dir: str,
+         train_size: float = 0.8,
+         val_size: float = 0.1,
+         balanced: bool = True,
+         background_label: int = 0,
+         channels_idx: int = 0):
+    """
+    :param data_file_path: Path to the data file. Supported types are: .npy
+    :param ground_truth_path: Path to the data file.
+    :param output_dir: Directory in which the output .h5 file will be stored
+    :type output_dir: Directory in which the prepared .h5 file will be stored
+    :param train_size: If float, should be between 0.0 and 1.0, if balanced = True, it represents percentage of each class to be extracted, 
+                       If float and balanced = False, it represents percetange of the whole dataset to be extracted with samples drawn randomly. 
+                       If int and balanced = True, it represents number of samples to be drawn from each class. 
+                       If int and balanced = False, it represents overall number of samples to be drawn, randomly. 
+                       Defaults to 0.8
+    :type train_size: float or int
+    :param val_size: Should be between 0.0 and 1.0. Represents the percentage of each class from the training set 
+                     to be extracted as a validation set, defaults to 0.1
+    :param balanced: Indicated whether the extracted training set should be balanced, defaults to True
+    :param background_label: Label indicating the background in GT file
+    :param channels_idx: Index specifing the dimensions of channels
+    :raises TypeError: When provided data or labels file is not supported
+    """
+    if data_file_path.endswith('.npy') and ground_truth_path.endswith('.npy'):
+        data, labels = load_npy(data_file_path, ground_truth_path)
+    else:
+        raise TypeError("The following data file type is not supported: {}".format(
+            os.path.splitext(data_file_path)[EXTENSION]))
+
+    data, labels = reshape_to_1D_samples(data, labels, channels_idx)
+    train_x, train_y, val_x, val_y, test_x, test_y = train_val_test_split(
+        data, labels, train_size, val_size, balanced, background_label)
+    
+    data_file = h5py.File(os.path.join(output_dir, 'pavia.h5'), 'w')
+    train_group = data_file.create_group(Dataset.TRAIN)
+    train_group.create_dataset(Dataset.DATA, data=train_x)
+    train_group.create_dataset(Dataset.LABELS, data=train_y)
+
+    val_group = data_file.create_group(Dataset.VAL)
+    val_group.create_dataset(Dataset.DATA, data=val_x)
+    val_group.create_dataset(Dataset.LABELS, data=val_y)
+    
+    test_group = data_file.create_group(Dataset.TEST)
+    test_group.create_dataset(Dataset.DATA, data=test_x)
+    test_group.create_dataset(Dataset.LABELS, data=test_y)
+    data_file.close()
+
+
+if __name__ == '__main__':
+    clize.run(main)
---
+++ utils.py
@@ -0,0 +1,103 @@
+import aenum
+from typing import Tuple, List, Union
+
+import numpy as np
+
+
+class Dataset(aenum.Constant):
+    TRAIN = 'train'
+    VAL = 'val'
+    TEST = 'test'
+    DATA = 'data'
+    LABELS = 'labels'
+
+
+def shuffle_arrays_together(arrays: List[np.ndarray], seed: int = 0):
+    """
+    Shuffle arbitrary number of arrays together, in-place
+
+    :param arrays: List of np.ndarrays to be shuffled
+    :param seed: seed for the random state, defaults to 0
+    :raises AssertionError: When provided arrays have different sizes along 
+                            first dimension
+    """
+    assert all(len(array) == len(arrays[0]) for array in arrays)
+    for array in arrays:
+        random_state = np.random.RandomState(seed)
+        random_state.shuffle(array)
+
+
+def train_val_test_split(data: np.ndarray, labels: np.ndarray,
+                         train_size: Union[int, float] = 0.8,
+                         val_size: float = 0.1,
+                         balanced: bool = True,
+                         background_label: int = 0) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Split the data into train, val and test sets. The size of the training set 
+    is set by the train_size paramter. All the remaining samples will be treated
+    as a test set
+
+    :param data: Data with the [SAMPLES, ...] dimensions
+    :param labels: Vector with corresponding labels
+    :param train_size: If float, should be between 0.0 and 1.0, if balanced = True, it represents percentage of each class to be extracted, 
+                       If float and balanced = False, it represents percetange of the whole dataset to be extracted with samples drawn randomly, 
+                            regardlesss of their class. 
+                       If int and balanced = True, it represents number of samples to be drawn from each class. 
+                       If int and balanced = False, it represents overall number of samples to be drawn regardless of their class, randomly. 
+                       Defaults to 0.8
+    :param val_size: Should be between 0.0 and 1.0. Represents the percentage of each class from the training set 
+                     to be extracted as a validation set, defaults to 0.1
+    :param balanced: Indicated whether the extracted training set should be balanced, defaults to True
+    :return: Three tuples: (train_x, train_y), (val_x, val_y), (test_x, test_y)
+    :raises TypeError: When wrong type is passed as train_size
+    """
+    data = data[labels != background_label]
+    labels = labels[labels != background_label]
+    shuffle_arrays_together([data, labels])
+    train_indices = _get_set_indices(labels, train_size, balanced)
+    train_x = data[train_indices]
+    train_y = labels[train_indices]
+    val_indices = _get_set_indices(train_y, val_size)
+    val_x = train_x[val_indices]
+    val_y = train_y[val_indices]
+    train_x = np.delete(train_x, val_indices, axis=0)
+    train_y = np.delete(train_y, val_indices)
+    data = np.delete(data, train_indices, axis=0)
+    labels = np.delete(labels, train_indices)
+    return train_x, train_y, val_x, val_y, data, labels
+
+
+def _get_set_indices(labels: np.ndarray, size: float = 0.8, balanced: bool = True) -> np.ndarray:
+    """
+    Extract indices of a subset of specified data according to size and
+    balanced parameters.
+
+    :param labels: Vector with corresponding labels
+    :param size: If float, should be between 0.0 and 1.0, if balanced = True, it represents percentage of each class to be extracted, 
+                       If float and balanced = False, it represents percetange of the whole dataset to be extracted with samples drawn randomly, 
+                            regardlesss of their class. 
+                       If int and balanced = True, it represents number of samples to be drawn from each class. 
+                       If int and balanced = False, it represents overall number of samples to be drawn regardless of their class, randomly. 
+                       Defaults to 0.8
+    :param balanced: Indicated whether the extracted training set should be balanced, defaults to True
+    :return: Indexes of the train set
+    :raises TypeError: When wrong type is passed as size
+    """
+    unique_labels = np.unique(labels)
+    label_indices = [np.where(labels == label)[0] for label in unique_labels]
+    if 0.0 < size < 1.0 and balanced is True:
+        for idx in range(len(unique_labels)):
+            samples_per_label = int(len(label_indices[idx]) * size)
+            label_indices[idx] = label_indices[idx][:samples_per_label]
+        train_indices = np.concatenate(label_indices, axis=0)
+    elif 0.0 < size < 1.0 and balanced is False:
+        train_indices = np.arange(int(len(labels) * size))
+    elif size > 1 and balanced is True:
+        for label in range(len(unique_labels)):
+            label_indices[label] = label_indices[label][:size]
+        train_indices = np.concatenate(label_indices, axis=0)
+    elif size > 1 and balanced is False:
+        train_indices = np.arange(size)
+    else:
+        raise TypeError("Wrong type of size argument passed")
+    return train_indices
