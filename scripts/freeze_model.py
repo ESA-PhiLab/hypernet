481cdb20c8b3d340836fc033e4102aef81f35be6
Michał Myller <mmyller@kplabs.pl>
2020-02-19 14:09:45 +0100

    added freeze_model.py script

---
+++ scripts/freeze_model.py
@@ -0,0 +1,45 @@
+"""
+Freeze the graph and store it in .pb (Protocol Buffers) format
+"""
+import os
+import warnings
+import json
+import clize
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from utils import freeze_session
+import tensorflow as tf
+from tensorflow import keras
+
+
+def main(*, model_path: str, output_dir: str):
+    """
+    :param model_path: Path to the model to be saved
+    :param output_dir: Directory in which the .pb graph and nodes json will be
+                       stored
+    """
+    keras.backend.set_learning_phase(0)
+    loaded_model = keras.models.load_model(model_path)
+
+    input_names = [out.op.name for out in loaded_model.inputs]
+    output_names = [out.op.name for out in loaded_model.outputs]
+
+    nodes = {'input_node': input_names[0],
+             'output_node': output_names[0]}
+
+    nodes = json.dumps(nodes, indent=4, sort_keys=True)
+
+    with open(os.path.join(output_dir, "freeze_input_output_node_name.json"),
+              "w+") as f:
+        f.write(nodes)
+
+    frozen_graph = freeze_session(keras.backend.get_session(),
+                                  output_names=output_names)
+    tf.train.write_graph(frozen_graph, output_dir, "frozen_graph.pb",
+                         as_text=False)
+    print("Frozen model saved at {}".format(output_dir))
+
+
+if __name__ == '__main__':
+    clize.run(main)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import aenum
 from typing import Tuple, List, Union
 
+import tensorflow as tf
 import numpy as np
 
 
@@ -31,7 +32,8 @@ def train_val_test_split(data: np.ndarray, labels: np.ndarray,
                          train_size: Union[int, float] = 0.8,
                          val_size: float = 0.1,
                          balanced: bool = True,
-                         background_label: int = 0) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+                         background_label: int = 0) -> Tuple[
+    np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Split the data into train, val and test sets. The size of the training set 
     is set by the train_size paramter. All the remaining samples will be treated
@@ -106,7 +108,8 @@ def _get_set_indices(labels: np.ndarray, size: float = 0.8,
 
 def reshape_to_1d_samples(data: np.ndarray,
                           labels: np.ndarray,
-                          channels_idx: int = 0) -> Tuple[np.ndarray, np.ndarray]:
+                          channels_idx: int = 0) -> Tuple[
+    np.ndarray, np.ndarray]:
     """
     Reshape the data and labels from [CHANNELS, HEIGHT, WIDTH] to [PIXEL, CHANNELS],
     so it fits the 1D Conv models
@@ -121,3 +124,35 @@ def reshape_to_1d_samples(data: np.ndarray,
     data = np.moveaxis(data, -1, 0)
     labels = labels.reshape(-1)
     return data, labels
+
+
+def freeze_session(session, keep_var_names=None, output_names=None,
+                   clear_devices=True):
+    """
+    Freezes the state of a session into a pruned computation graph.
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    :param session: The TensorFlow session to be frozen.
+    :param keep_var_names: A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    :param output_names: Names of the relevant graph outputs.
+    :param clear_devices: Remove the device directives from the graph for better
+                          portability.
+    :return The frozen graph definition.
+    """
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(
+            set(v.op.name for v in tf.global_variables()).difference(
+                keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = tf.graph_util.convert_variables_to_constants(
+            session, input_graph_def, output_names, freeze_var_names)
+    return frozen_graph
