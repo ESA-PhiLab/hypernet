911631c63174562f851b0078ecfa05e1775c480b
Michał Myller <mmyller@kplabs.pl>
2020-03-10 15:27:49 +0100

    working version of the evaluate_graph script

--- ml_intuition/data/input_fn.py
+++ ml_intuition/data/input_fn.py
@@ -1,7 +1,7 @@
 """
 File containing functions which calibrate input for frozen graphs used for
 quantization. All arguments to such functions are passed through the shell
-environment.
+environment, because they are executed through the internal decent tools.
 """
 import os
 from typing import Dict
@@ -20,17 +20,17 @@ def calibrate_2d_input(iter: int) -> Dict[str, np.ndarray]:
     Return dictionary with a batch of the training data to be used for
     quantization calibration. One dimension and the end is added and the
     min max normalization is performed.
-    :param iter: Int object indicating the calibration step number
+    :param iter: Int object indicating the calibration step
     :return: Dict with name of the input node as key and training samples as
              np.ndarray
     """
     batch_start = iter * batch_size
     batch_end = iter * batch_size + batch_size
     with h5py.File(dataset, 'r') as file:
-        train_data = file[utils.Dataset.TRAIN][utils.Dataset.DATA][()]
-        train_data = train_data[batch_start:batch_end]
-        train_data = np.expand_dims(train_data, axis=-1)
-        train_min, train_max = file.attrs[utils.DataStats.MIN], \
+        samples = file[utils.Dataset.TRAIN][utils.Dataset.DATA][()]
+        samples = samples[batch_start:batch_end]
+        samples = np.expand_dims(samples, axis=-1)
+        min_value, max_value = file.attrs[utils.DataStats.MIN], \
                                file.attrs[utils.DataStats.MAX]
-    train_data = (train_data - train_min) / (train_max - train_min)
-    return {input_node_name: train_data}
+    samples = (samples - min_value) / (max_value - min_value)
+    return {input_node_name: samples}
--- scripts/evaluate_graph.py
+++ scripts/evaluate_graph.py
@@ -1,8 +1,40 @@
 import clize
+import h5py
+import json
+import numpy as np
+
+import tensorflow as tf
+import tensorflow.contrib.decent_q
+from sklearn.metrics import accuracy_score
 
 import ml_intuition.data.io as io
+import ml_intuition.data.utils as utils
 
 
-def main(*, graph_path: str, dataset_path: str):
+def main(*, graph_path: str, node_names_path: str, dataset_path: str):
+
     graph = io.load_pb(graph_path)
-    
+
+    with h5py.File(dataset_path, 'r') as file:
+        test_data = file[utils.Dataset.TEST][utils.Dataset.DATA][:]
+        test_labels = file[utils.Dataset.TEST][utils.Dataset.LABELS][:]
+        min_value, max_value = file.attrs[utils.DataStats.MIN], \
+                               file.attrs[utils.DataStats.MAX]
+
+    test_data = (test_data - min_value) / (max_value - min_value)
+    test_data = np.expand_dims(test_data, axis=-1)
+
+    with open(node_names_path, 'r') as node_names_file:
+        node_names = json.loads(node_names_file.read())
+
+    input_node = graph.get_tensor_by_name(node_names['input_node'] + ':0')
+    output_node = graph.get_tensor_by_name(node_names['output_node'] + ':0')
+
+    with tf.Session(graph=graph) as session:
+        predictions = session.run(output_node, feed_dict={input_node: test_data})
+        predictions = session.run(tf.argmax(predictions, axis=-1))
+    print('acc: {}'.format(accuracy_score(test_labels, predictions)))
+
+
+if __name__ == '__main__':
+    clize.run(main)
