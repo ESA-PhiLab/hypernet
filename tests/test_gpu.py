9dc7341277a48f5420c33990a8ca6186669f0856
Michał Myller <mmyller@kplabs.pl>
2020-05-04 15:21:47 +0200

    added gpu tests

---
+++ tests/test_gpu.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+class TestGPU:
+    def test_run_on_gpu(self):
+        with tf.device('/gpu:0'):
+            a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3],
+                            name='a')
+            b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2],
+                            name='b')
+            c = tf.matmul(a, b)
+        with tf.Session() as sess:
+            print(sess.run(c))
+
+        sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+        print(sess)
