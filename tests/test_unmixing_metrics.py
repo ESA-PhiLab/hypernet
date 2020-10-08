afcafcadcabea1a6b93d052559477c6f66e48813
Lukasz <luktul13@gmail.com>
2020-10-08 10:26:20 +0200

    Added tests to unmixing second paper's metrics.

--- tests/test_unmixing_metrics.py
+++ tests/test_unmixing_metrics.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from sklearn.metrics import mean_squared_error
 
 from ml_intuition.evaluation.performance_metrics import overall_rms_abundance_angle_distance, \
-    cnn_rmse, per_class_rmse
+    cnn_rmse, per_class_rmse, dcae_rmse, average_angle_spectral_mapper
 
 sess = tf.Session()
 
@@ -43,14 +43,46 @@ class TestRMSE:
             (np.random.uniform(0, 1, (10, 2)), np.random.uniform(0, 1, (10, 2))),
             (np.random.uniform(0, 1, (10, 1)), np.random.uniform(0, 1, (10, 1)))
         ])
-    def test_rmse_with_external_implementations(self, y_true: np.ndarray, y_pred: np.ndarray):
+    def test_cnn_rmse_with_external_implementations(self, y_true: np.ndarray, y_pred: np.ndarray):
         y_true, y_pred = softmax(y_true), softmax(y_pred)
         tf_y_true, tf_y_pred = tf.convert_to_tensor(y_true), tf.convert_to_tensor(y_pred)
         tf_overall_error = cnn_rmse(y_true=tf_y_true, y_pred=tf_y_pred).eval(session=sess)
         error = np.sqrt(np.mean((y_true - y_pred) ** 2))
         assert round(tf_overall_error, 3) == round(error, 3) == \
                round(np.sqrt(mean_squared_error(y_true=y_true, y_pred=y_pred)), 3)
-
         tf_per_class_error = per_class_rmse(y_true=tf_y_true, y_pred=tf_y_pred).eval(session=sess)
         per_class_error = np.sqrt(np.mean((y_true - y_pred) ** 2, axis=0))
         assert np.array_equal(np.round(tf_per_class_error, 3), np.round(per_class_error, 3))
+
+    @pytest.mark.parametrize(
+        'y_true, y_pred',
+        [
+            (np.random.uniform(0, 1, (10, 10)), np.random.uniform(0, 1, (10, 10))),
+            (np.random.uniform(0, 1, (10, 2)), np.random.uniform(0, 1, (10, 2))),
+            (np.random.uniform(0, 1, (10, 1)), np.random.uniform(0, 1, (10, 1)))
+        ])
+    def test_dcae_rmse_with_external_implementations(self, y_true: np.ndarray, y_pred: np.ndarray):
+        y_true, y_pred = softmax(y_true), softmax(y_pred)
+        tf_y_true, tf_y_pred = tf.convert_to_tensor(y_true), tf.convert_to_tensor(y_pred)
+        tf_overall_error = dcae_rmse(y_true=tf_y_true, y_pred=tf_y_pred).eval(session=sess)
+        error = np.mean(np.sqrt(np.mean(np.square(y_pred - y_true), axis=1)))
+        assert round(tf_overall_error, 3) == round(error, 3)
+
+
+class TestAverageAngleSpectralMapper:
+    @pytest.mark.parametrize(
+        'y_true, y_pred',
+        [
+            (np.random.uniform(0, 1, (10, 10)), np.random.uniform(0, 1, (10, 10))),
+            (np.random.uniform(0, 1, (10, 2)), np.random.uniform(0, 1, (10, 2))),
+            (np.random.uniform(0, 1, (10, 1)), np.random.uniform(0, 1, (10, 1)))
+        ])
+    def test_aSAM_with_external_implementations(self, y_true: np.ndarray, y_pred: np.ndarray):
+        y_true, y_pred = softmax(y_true), softmax(y_pred)
+        tf_y_true, tf_y_pred = tf.convert_to_tensor(y_true), tf.convert_to_tensor(y_pred)
+        tf_error = average_angle_spectral_mapper(y_true=tf_y_true, y_pred=tf_y_pred).eval(session=sess)
+        error = 0
+        for i in range(y_true.shape[0]):
+            error += np.arccos(np.dot(y_true[i], y_pred[i]) / (np.linalg.norm(y_true[i]) * np.linalg.norm(y_pred[i])))
+        error /= y_true.shape[0]
+        assert round(tf_error, 3) == round(error, 3)
