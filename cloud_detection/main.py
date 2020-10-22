343b9e918fcc9b497dcc63d5b325524418c854e0
Bartosz Grabowski <bgrabowski@kplabs.pl>
2020-10-22 21:12:31 +0200

    Add MLFlow

--- cloud_detection/main.py
+++ cloud_detection/main.py
@@ -1,6 +1,8 @@
 """ Functions to train and test the models for cloud detection. """
 
 import matplotlib.pyplot as plt
+import mlflow
+import mlflow.tensorflow
 from typing import Dict
 from pathlib import Path
 from tensorflow import keras
@@ -14,6 +16,10 @@ def main(c: Dict):
 
     :param c: Dict of params.
     """
+    # Configure MLFlow
+    mlflow.set_tracking_uri("http://beetle.mlflow.kplabs.pl")
+    mlflow.set_experiment("cloud_detection")
+    mlflow.log_params(c)
     # Load data
     train_files, test_files = load_image_paths(
         c["dpath"],
@@ -33,10 +39,11 @@ def main(c: Dict):
                 bn_momentum=c["bn_momentum"]
                 )
     model.compile(
-        optimizer=keras.optimizers.Adam(lr=c["lr"]),
+        optimizer=keras.optimizers.Adam(lr=c["learning_rate"]),
         loss='categorical_crossentropy',
         metrics=[keras.metrics.categorical_accuracy]
         )
+    mlflow.tensorflow.autolog(every_n_iter=1)
     # Train model
     model.fit_generator(
         generator=traingen,
@@ -50,18 +57,17 @@ def main(c: Dict):
         steps=c["steps_per_epoch"],
         verbose=1
         )
-    print(f"Test loss: {test_loss}")
-    print(f"Test acc: {test_acc}")
-    # Show sample prediction
-    x, y = next(iter(testgen))
-    y_pred = model.predict(x)
-    for i in range(len(x)):
-        plt.figure()
-        plt.imshow(y[i, :, :, 1])
-        plt.show()
-        plt.figure()
-        plt.imshow(y_pred[i, :, :, 1])
-        plt.show()
+    mlflow.log_metrics({"test loss": test_loss, "test acc": test_acc})
+    # # Show sample prediction
+    # x, y = next(iter(testgen))
+    # y_pred = model.predict(x)
+    # for i in range(len(x)):
+    #     plt.figure()
+    #     plt.imshow(y[i, :, :, 1])
+    #     plt.show()
+    #     plt.figure()
+    #     plt.imshow(y_pred[i, :, :, 1])
+    #     plt.show()
 
 
 if __name__ == "__main__":
@@ -69,9 +75,9 @@ if __name__ == "__main__":
         "dpath": Path("../datasets/clouds/38-Cloud/38-Cloud_training"),
         "train_size": 0.1,
         "batch_size": 8,
-        "lr": .01,
+        "learning_rate": .01,
         "bn_momentum": .9,
-        "epochs": 100,
-        "steps_per_epoch": 100
+        "epochs": 5,
+        "steps_per_epoch": 2
         }
     main(params)
