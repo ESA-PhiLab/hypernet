f1e5fe9bc261f2359e6e47d8b99267597e74704f
Bartosz Grabowski <bgrabowski@kplabs.pl>
2020-11-03 20:49:08 +0100

    style

--- cloud_detection/utils.py
+++ cloud_detection/utils.py
@@ -12,7 +12,7 @@ def overlay_mask(image: np.ndarray, mask: np.ndarray, overlay_intensity: float=0
 
 
 def setup_mlflow(c):
-     mlflow.set_tracking_uri("http://beetle.mlflow.kplabs.pl")
-     mlflow.set_experiment("cloud_detection")
-     mlflow.tensorflow.autolog(every_n_iter=1)
-     mlflow.log_params(c)
+    mlflow.set_tracking_uri("http://beetle.mlflow.kplabs.pl")
+    mlflow.set_experiment("cloud_detection")
+    mlflow.tensorflow.autolog(every_n_iter=1)
+    mlflow.log_params(c)
