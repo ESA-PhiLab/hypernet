290037c14fe623e46de59b9ac7f9393dd201f871
Maciej Ziaja <mziaja@kplabs.pl>
2020-11-10 16:46:53 +0100

    Add jaccard metrics tests

---
+++ cloud_detection/tests/test_metrics.py
@@ -0,0 +1,120 @@
+import tensorflow.keras.backend as K
+
+
+def Jaccard_index_loss(smooth: float = 0.0000001):
+    '''
+    Jaccard index training loss for segmentation like tasks. 
+    Default smoothness coefficient comes from Cloud-Net example.
+    '''
+
+    def jaccard_index_loss(y_true, y_pred):
+        intersection = K.sum(y_true * y_pred, axis=(1, 2))
+        y_true_sum = K.sum(y_true, axis=(1, 2))
+        y_pred_sum = K.sum(y_pred, axis=(1, 2))
+
+        jaccard = (intersection + smooth) / (y_true_sum + y_pred_sum - intersection + smooth)
+        return 1 - jaccard
+
+    return jaccard_index_loss
+
+
+def Jaccard_index_metric(smooth: float = 0.0000001):
+
+    def jaccard_index_metric(y_true, y_pred):
+        intersection = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)), axis=(1, 2))
+        y_true_sum = K.sum(K.round(y_true), axis=(1, 2))
+        y_pred_sum = K.sum(K.round(y_pred), axis=(1, 2))
+
+        return (intersection + smooth) / (y_true_sum + y_pred_sum - intersection + smooth)
+
+    return jaccard_index_metric
+
+
+def Dice_coef_metric():
+    '''
+    Dice coefficient training loss for segmentation like tasks.
+    Internally uses Jaccard index.
+    '''
+    ji = Jaccard_index_metric()
+
+    def dice_coeff_metric(y_true, y_pred):
+        ji_score = ji(y_true, y_pred)
+        return 2 * ji_score / (ji_score + 1)
+
+    return dice_coeff_metric
+
+
+def recall(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    ret = true_positives / (possible_positives + K.epsilon())
+    return ret
+
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    ret = true_positives / (predicted_positives + K.epsilon())
+    return ret
+
+
+def specificity(y_true, y_pred):
+    y_true_neg = 1 - y_true
+    y_pred_neg = 1 - y_pred
+    true_negatives = K.sum(K.round(K.clip(y_true_neg * y_pred_neg, 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(y_true_neg, 0, 1)))
+    ret = true_negatives / (possible_negatives + K.epsilon())
+    return ret
+
+
+def f1_score(y_true, y_pred):
+    prec = precision(y_true, y_pred)
+    rec = recall(y_true, y_pred)
+    return 2 * ((prec*rec) / (prec + rec + K.epsilon()))
+
+###########
+import pytest
+import numpy as np
+from tensorflow.debugging import assert_near
+from tensorflow import errors
+
+
+@pytest.mark.parametrize(
+    'y_true, y_pred, expected',
+    [
+        (K.constant([[ [1, 1], [1, 1] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(0)),
+        (K.constant([[ [0, 1], [1, 1] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(0.25)),
+        (K.constant([[ [0, 1], [0, 1] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(0.5)),
+        (K.constant([[ [0, 0], [0, 1] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(0.75)),
+        (K.constant([[ [0, 0], [0, 0] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(1)),
+        (K.constant([[ [1, 1], [0, 0] ]]), K.constant([[ [0.25, 0.25], [0, 0] ]]), K.constant(0.75)),
+        (K.constant([[ [1, 1], [0, 0] ]]), K.constant([[ [0.75, 0.75], [0, 0] ]]), K.constant(0.25)),
+    ]
+)
+def test_jaccard_index_loss(y_true, y_pred, expected):
+    jaccard = Jaccard_index_loss()
+    try:
+        assert_near(jaccard(y_true, y_pred), expected)
+    except errors.InvalidArgumentError as e:
+        pytest.fail(e.message, pytrace=False)
+
+
+@pytest.mark.parametrize(
+    'y_true, y_pred, expected',
+    [
+        (K.constant([[ [1, 1], [1, 1] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(1)),
+        (K.constant([[ [0, 1], [1, 1] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(0.75)),
+        (K.constant([[ [0, 1], [0, 1] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(0.5)),
+        (K.constant([[ [0, 0], [0, 1] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(0.25)),
+        (K.constant([[ [0, 0], [0, 0] ]]), K.constant([[ [1, 1], [1, 1] ]]), K.constant(0)),
+        (K.constant([[ [1, 1], [0, 0] ]]), K.constant([[ [0.25, 0.25], [0, 0] ]]), K.constant(0)),
+        (K.constant([[ [1, 1], [0, 0] ]]), K.constant([[ [0.75, 0.75], [0, 0] ]]), K.constant(1)),
+    ]
+)
+def test_jaccard_index_metric(y_true, y_pred, expected):
+    jaccard = Jaccard_index_metric()
+    try:
+        assert_near(jaccard(y_true, y_pred), expected)
+    except errors.InvalidArgumentError as e:
+        pytest.fail(e.message, pytrace=False)
+
