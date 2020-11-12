93ebc6ec436511baaf65981ceec6c6c42c7f7c7b
Michał Myller <mmyller@kplabs.pl>
2020-11-12 11:53:20 +0100

    added iniital version of Ensemble classifier

--- ml_intuition/models.py
+++ ml_intuition/models.py
@@ -3,8 +3,11 @@ All models that are used for training.
 """
 
 import sys
+from typing import Union, List
 
+import numpy as np
 import tensorflow as tf
+from scipy.stats import mode
 
 
 def model_2d(kernel_size: int,
@@ -27,19 +30,23 @@ def model_2d(kernel_size: int,
                                          input_shape=(input_size, 1, 1),
                                          padding="valid",
                                          activation='relu'))
-        model.add(tf.keras.layers.Conv2D(n_kernels, (kernel_size, 1), strides=(3, 1),
-                                         input_shape=(input_size, 1, 1),
-                                         padding="valid",
-                                         activation='relu'))
-        model.add(tf.keras.layers.Conv2D(n_kernels, (kernel_size, 1), strides=(2, 1),
-                                         input_shape=(input_size, 1, 1),
-                                         padding="valid",
-                                         activation='relu'))
-        model.add(tf.keras.layers.Conv2D(n_kernels, (kernel_size, 1), strides=(2, 1),
-                                         input_shape=(input_size, 1, 1),
-                                         padding="valid",
-                                         activation='relu'))
+        model.add(
+            tf.keras.layers.Conv2D(n_kernels, (kernel_size, 1), strides=(3, 1),
+                                   input_shape=(input_size, 1, 1),
+                                   padding="valid",
+                                   activation='relu'))
+        model.add(
+            tf.keras.layers.Conv2D(n_kernels, (kernel_size, 1), strides=(2, 1),
+                                   input_shape=(input_size, 1, 1),
+                                   padding="valid",
+                                   activation='relu'))
+        model.add(
+            tf.keras.layers.Conv2D(n_kernels, (kernel_size, 1), strides=(2, 1),
+                                   input_shape=(input_size, 1, 1),
+                                   padding="valid",
+                                   activation='relu'))
         return model
+
     model = tf.keras.Sequential()
 
     for _ in range(n_layers):
@@ -75,6 +82,7 @@ def pool_model_2d(kernel_size: int,
         model.add(tf.keras.layers.BatchNormalization()),
         model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 1), strides=(1, 1)))
         return model
+
     model = tf.keras.Sequential()
 
     for _ in range(n_layers):
@@ -89,7 +97,7 @@ def pool_model_2d(kernel_size: int,
 def get_model(model_key: str, kernel_size: int, n_kernels: int,
               n_layers: int, input_size: int, n_classes: int):
     """
-    Get a given instance of model specified by mode_key.
+    Get a given instance of model specified by model_key.
 
     :param model_key: Specifies which model to use.
     :param kernel_size: Size of the convolutional kernel.
@@ -105,3 +113,52 @@ def get_model(model_key: str, kernel_size: int, n_kernels: int,
     return all_[model_key](kernel_size=kernel_size,
                            n_kernels=n_kernels, n_layers=n_layers,
                            input_size=input_size, n_classes=n_classes)
+
+
+class Ensemble:
+    def __init__(self, models: Union[List[tf.keras.Sequential], List[str]],
+                 voting: str = 'hard'):
+        """
+        Ensemble for using multiple models for prediction
+        :param models: Either list of tf.keras.models.Sequential models,
+            or a list of paths to the models (can't mix both).
+        :param voting: If ‘hard’, uses predicted class labels for majority rule
+            voting. Else if ‘soft’, predicts the class label based on the argmax
+            of the sums of the predicted probabilities.
+        """
+        if all(type(model) is str for model in models):
+            self.models = [tf.keras.models.load_model(model) for model in
+                           models]
+        elif all(type(model) is tf.keras.models.Sequential for model in models):
+            self.models = models
+        else:
+            raise TypeError("Wrong type of models provided, pass either path "
+                            "or the model itself")
+        self.voting = voting
+
+    def predict(self, data: Union[np.ndarray, List[np.ndarray]],
+                batch_size: int = 1024) -> np.ndarray:
+        """
+        Return predicted classes
+        :param data: Either a single dataset which will be fed into all of the
+            models, or a list of datasets, unique for each model
+        :param batch_size: Size of the batch used for prediction
+        :return: Predicted classes
+        """
+        predictions = []
+        if type(data) is list:
+            for model, dataset in zip(self.models, data):
+                predictions.append(model.predict(dataset,
+                                                 batch_size=batch_size))
+            predictions = np.array(predictions)
+        else:
+            for model in self.models:
+                predictions.append(model.predict(data, batch_size=batch_size))
+            predictions = np.array(predictions)
+        if self.voting == 'hard':
+            predictions = np.argmax(predictions, axis=-1)
+            return mode(predictions, axis=0)[0][:, 0]
+        elif self.voting == 'soft':
+            predictions = np.sum(predictions, axis=0)
+            predictions = np.argmax(predictions, axis=-1)
+        return predictions
