0d4d2defcd03002a1dff51810931901e7b4f70e6
Bartosz Grabowski <bgrabowski@kplabs.pl>
2020-11-30 21:59:12 +0100

    Add 38-Cloud class balancing

--- cloud_detection/data_gen.py
+++ cloud_detection/data_gen.py
@@ -12,6 +12,7 @@ from typing import Dict, List, Tuple
 
 from utils import overlay_mask, pad
 
+
 def strip_nir(hyper_img: np.ndarray) -> np.ndarray:
     """
     Strips nir channel so image can be displayed.
@@ -105,6 +106,7 @@ class DG_38Cloud(keras.utils.Sequence):
     def __init__(self,
                  files,
                  batch_size: int,
+                 balance_classes: bool=False,
                  dim: Tuple[int, int]=(384, 384),
                  shuffle: bool=True,
                  with_gt: bool=True
@@ -113,6 +115,7 @@ class DG_38Cloud(keras.utils.Sequence):
         Prepare generator and init paths to files containing image channels.
         param batch_size: size of generated batches, only one batch is loaded
               to memory at a time.
+        param balance_classes: if True balance classes.
         param dim: Tuple with x, y image dimensions.
         param shuffle: if True shuffles dataset on each epoch end.
         param with_gt: if True returns y along with x.
@@ -121,9 +124,41 @@ class DG_38Cloud(keras.utils.Sequence):
         self._dim: Tuple[int, int] = dim
         self._shuffle: bool = shuffle
         self._with_gt: bool = with_gt
+        self._balance_classes: bool = balance_classes
 
         self._files = files
         self._file_indexes = np.arange(len(self._files))
+
+        if self._balance_classes:
+            self._balance_file_indexes()
+
+
+    def _balance_file_indexes(self):
+        """
+        Upsamples the file indexes of the smaller class.
+        """
+        labels = self._get_labels_for_balancing()
+        pos_idx = self._file_indexes[np.array(labels, dtype=bool)]
+        neg_idx = self._file_indexes[~np.array(labels, dtype=bool)]
+        if len(pos_idx) < len(neg_idx):
+            resampled_idx = np.random.choice(pos_idx, len(neg_idx))
+            self._file_indexes = np.concatenate([neg_idx, resampled_idx], axis=0)
+        elif len(pos_idx) > len(neg_idx):
+            resampled_idx = np.random.choice(neg_idx, len(pos_idx))
+            self._file_indexes = np.concatenate([pos_idx, resampled_idx], axis=0)
+        self._file_indexes = np.sort(self._file_indexes)
+
+
+    def _get_labels_for_balancing(self) -> List[int]:
+        """
+        Returns the pseudo-labels for each patch. Pseudo-label being
+        0 if more pixels in the patch are non-clouds, and 1 otherwise.
+        """
+        labels = []
+        for file_ in self._files:
+            gt = self._open_mask(file_)
+            labels.append(int(np.median(gt)))
+        return labels
 
 
     def _open_as_array(self, channel_files: Dict[str, Path]) -> np.ndarray:
--- cloud_detection/main.py
+++ cloud_detection/main.py
@@ -15,6 +15,7 @@ def main(c):
     if c["mlflow"] == True:
         setup_mlflow(c)
     model = train_model(c["train_path"], c["train_size"], c["batch_size"],
+                        c["balance_train_dataset"], c["balance_val_dataset"],
                         c["bn_momentum"], c["learning_rate"], c["stopping_patience"],
                         c["steps_per_epoch"], c["epochs"])
     metrics_38Cloud = test_38Cloud(model, c["38Cloud_path"], c["38Cloud_gtpath"], c["vpath"], c["vids"], c["batch_size"])
@@ -50,6 +51,8 @@ if __name__ == "__main__":
         "vids": ('*'),
         "train_size": 0.8,
         "batch_size": 8,
+        "balance_train_dataset": True,
+        "balance_val_dataset": False,
         "learning_rate": .01,
         "bn_momentum": .9,
         "epochs": 200,
