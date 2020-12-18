9437b4c904240efb9c07dc71cde2f41173bfa0c0
Michał Myller <mmyller@kplabs.pl>
2020-12-18 11:33:56 +0100

    added installation to docs

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 autoclass_content = 'both'
+pdf_use_index = False
