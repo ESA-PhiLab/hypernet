afd946594d4d90076a908b65a85e041c6b6397c5
Michał Myller <mmyller@kplabs.pl>
2021-01-25 12:59:38 +0100

    added config for session for RTX card

--- scripts/inference_runner.py
+++ scripts/inference_runner.py
@@ -9,6 +9,9 @@ import re
 import clize
 import mlflow
 import tensorflow as tf
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
 from clize.parameters import multi
 
 from scripts import evaluate_model, prepare_data, artifacts_reporter
